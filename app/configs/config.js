328d413719cd258930bfa86d3dc1a713f20a2f15
Tony Brown <tonyxbrown@gmail.com>
2015-12-07 12:35:16 -0800

    seeing if config change works on qa

--- app/configs/config.js
+++ app/configs/config.js
@@ -2,6 +2,6 @@ window.configOptions = {
   'environment': 'dev',
   'ajaxURL': 'https://qa-api.wetdesign.com/feedback',
   'html2canvasURL': '../plugins/feedbackjs/html2canvas.js',
-  'base_url': 'https://qa-api.wetdesign.com/mf',
+  'base_url': 'https://qa-api.wetdesign.com/mf/',
   'loglevel': 3
 }
