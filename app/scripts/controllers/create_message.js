b8b408d38da1ebe9840e93dec7f04bc19c5f7b23
Tony Brown <tonyxbrown@gmail.com>
2015-12-08 13:06:02 -0800

    logic to show additional languages in create message

--- app/scripts/controllers/create_message.js
+++ app/scripts/controllers/create_message.js
@@ -10,6 +10,10 @@
 angular.module('messageFactoryApp')
   .controller('CreateMessageCtrl', ['$scope', '$window', '$location', '$timeout', '$filter', 'config_ui', 'MFAPIService', function ($scope, $window, $location, $timeout, $filter, config_ui, MFAPIService) {
 
+    $scope.additionalLanguages = [];
+    $scope.selectedExternalMessages = {};
+    $scope.selectedLanguages = {};
+
     $scope.loadPageOptions = function() {
       MFAPIService.getAppNames().then(function(result) {
         console.log("result:",result);
@@ -26,6 +30,12 @@ angular.module('messageFactoryApp')
     };
 
     $scope.loadPageOptions();
+
+    $scope.addAnotherLanguage = function() {
+      console.log("addAnotherLanguage()");
+      var lang_id = $scope.additionalLanguages.length + 1;
+      $scope.additionalLanguages.push(lang_id);
+    };
 
     $scope.newMessageSubmit = function() {
 
@@ -46,6 +56,19 @@ angular.module('messageFactoryApp')
           "messageLevel": $scope.selectedMessageLevel,
           "language": languageToUse
         };
+
+        // Additional Languages logic - append to messageToPost
+        if ($scope.selectedLanguages) {
+          var count = 0;
+          for (var obj in $scope.additionalLanguages) {
+            console.log("add lang for: ",obj);
+            if ($scope.selectedLanguages[(count+1).toString()] && $scope.selectedExternalMessages[(count+1).toString()]) {
+              messageToPost[$scope.languages[$scope.selectedLanguages[(count+1).toString()]]] = $scope.selectedExternalMessages[(count+1).toString()];
+            }
+            count++;
+          }
+        }
+
         MFAPIService.createMessages([messageToPost]).then(function(result) {
           console.log("createMessages call returned. result: ",result);
           $scope.appResults = result.data;
@@ -64,51 +87,5 @@ angular.module('messageFactoryApp')
         console.log("Do not have required form data to submit new Message");
       }
     };
-
-    //$scope.closeProgressModal = function() {
-    //  $('#progressModal').modal('hide');
-    //};
-    //
-    //$('#progressModal').on('show.bs.modal', function (event) {
-    //  var button = $(event.relatedTarget); // Button that triggered the modal
-    //  var po = button.data('po'); // po number
-    //  var collection = POAPIService.getPOByNumber(ctrl.poCollection,po);
-    //  var approvalHistory = collection.approval_history;
-    //  console.log("collection:",collection);
-    //
-    //  var progressHTML = "";
-    //  for (var i=0; i<approvalHistory.length; i++) {
-    //    var approvalPercent = (approvalHistory[i].approved === "3") ? 100 : (parseInt(approvalHistory[i].approved) * 30);
-    //    if (approvalPercent === 0 || !approvalPercent) { approvalPercent = 2; }
-    //    var d = new Date(parseInt(approvalHistory[i].timestamp));
-    //    var approvalDate = d.toLocaleString();
-    //
-    //    progressHTML += "<div class='progress'>";
-    //    if (approvalHistory[i].status.toLowerCase().indexOf("decline") !== -1) {
-    //      progressHTML += "<div class='progress-bar progress-bar-danger' role='progressbar' ";
-    //    }
-    //    else if (approvalHistory[i].status.toLowerCase().indexOf("approve") !== -1) {
-    //      progressHTML += "<div class='progress-bar progress-bar-success' role='progressbar' ";
-    //    }
-    //    else if (approvalHistory[i].status.toLowerCase().indexOf("recommitted") !== -1) {
-    //      progressHTML += "<div class='progress-bar progress-bar-warning' role='progressbar' ";
-    //    }
-    //    else {
-    //      progressHTML += "<div class='progress-bar' role='progressbar' ";
-    //    }
-    //    progressHTML += "aria-valuenow='"+ approvalPercent + "' ";
-    //    progressHTML += "aria-valuemin='0' aria-valuemax='100' ";
-    //    progressHTML += "style='width: "+ approvalPercent +"%;' >";
-    //    progressHTML += "</div>";
-    //    progressHTML += "</div>";
-    //    progressHTML += "<p class='help-block'>" + approvalHistory[i].status + " by " + approvalHistory[i].approval_name + " - " + approvalDate + "</p>";
-    //  }
-    //
-    //  var modal = $(this);
-    //
-    //  modal.find('.progress-modal-title').html("Approval Progress History - " + po);
-    //  modal.find('.progress-container').html(progressHTML);
-    //
-    //});
 
   }]);
