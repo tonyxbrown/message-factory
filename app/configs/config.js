8a2e45bcd770ace4af9b25d569fbfa5ac60dfa14
Tony Brown <tonyxbrown@gmail.com>
2015-12-08 10:05:15 -0800

    try to fix config for plugins

--- app/configs/config.js
+++ app/configs/config.js
@@ -1,7 +1,7 @@
 window.configOptions = {
   'environment': 'dev',
   'ajaxURL': 'https://qa-api.wetdesign.com/feedback',
-  'html2canvasURL': '../plugins/feedbackjs/html2canvas.js',
+  'html2canvasURL': 'plugins/feedbackjs/html2canvas.js',
   'base_url': 'https://qa-api.wetdesign.com/mf/',
   'loglevel': 3
 }
